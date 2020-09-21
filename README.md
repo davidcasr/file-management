83023fc0756426b04b0ae63350a2cda2de6de36d
davidcasr <david.castroruiz@gmail.com>
2020-09-21 14:06:44 -0500

    Initial commit | Basic management file

---
+++ README.md
@@ -0,0 +1,3 @@
+# File Management
+
+This repository is an exercise for management files, import files and export files, flat and .xls format
---
+++ files/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class File(models.Model):
+	name 				= models.CharField(max_length=50, null=False, blank=True)
+	file     			= models.FileField(upload_to="files", null=False, blank=True)
+
+	def __str__(self):
+		return self.name
